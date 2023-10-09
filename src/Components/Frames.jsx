3dcec4560429374c11f2faca7ed7b8c2705c428e
Angel <jangelolvera@outlook.com>
2023-10-09 13:28:44 -0600

    Experiencia cards

---
+++ src/Components/Frames.jsx
@@ -0,0 +1,32 @@
+import "../Styles/Frames.css";
+
+export const Frames = () => {
+  return (
+    <>
+      <div id="frameContainer">
+        <div className="Frame" id="frame1"></div>
+
+        <div className="Frame" id="frame2">
+          <span className="cardsTxt">
+           
+            Experiencia en perforación y reparación de pozos
+          </span>
+        </div>
+
+        <div className="Frame" id="frame3">
+          <span className="cardsTxt">
+            Trabajos de performance en integración direccional-barrenas
+          </span>
+        </div>
+
+        <div className="Frame" id="frame4">
+          <span id="cardsTxtBlack">
+            Aplicación de tecnologías: RSS, LWD y giroscópicos
+          </span>
+        </div>
+
+        <div className="Frame" id="frame5"></div>
+      </div>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,23 @@ import { NavBar } from './Components/NavBar';
 import { Home } from './Components/Home';
 import {AcercaDe} from './Components/AcercaDe';
 import {Servicios} from './Components/Servicios';
+import {Frames} from './Components/Frames';
+import { Nav } from 'react-bootstrap';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
-    <NavBar></NavBar>
-    <Home></Home>
-    <AcercaDe></AcercaDe>
-    <Servicios></Servicios>
+    <NavBar/>
+
+    <Home/>
+
+    <AcercaDe/>
+
+    <Servicios/>
+
+    <Frames/>
+
     
   </React.StrictMode>,
 )
