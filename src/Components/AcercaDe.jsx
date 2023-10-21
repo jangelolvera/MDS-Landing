dbf755157501e27598aa3ef95280373d3b1ad21d
Angel <jangelolvera@outlook.com>
2023-10-20 20:16:28 -0600

    responsive corrected

--- src/Components/AcercaDe.jsx
+++ src/Components/AcercaDe.jsx
@@ -4,6 +4,7 @@ import barco from '../Images/barco.jpg';
 import workers from '../Images/workers.png';
 import pozo from '../Images/pozo.jpg';
 import '../Styles/AcercaDe.css';
+import eficaz from '../Images/eficaz.jpg';
 
 export const AcercaDe = () => {
   return (
@@ -14,15 +15,14 @@ export const AcercaDe = () => {
         <Carousel.Item>
           <img className="slideImgs" src={barrena} alt="First slide" />
           <Carousel.Caption>
-            <h3 className="captionSlide">Contamos con una gran variedad tecnologías de alto desempeño, disponibilidad de diferentes 
-              tipos de herramientas y proveedores que se adaptan a las necesidades del cliente.</h3>
+            <h3 className="captionSlide">Contamos con una gran variedad de proveedores, tecnologías y herramientas de alto desempeño 
+            que se adaptan a las necesidades del cliente.</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="slideImgs" src={barco} alt="First slide" />
           <Carousel.Caption>
-            <h3 className="captionSlide">Contamos con un sistema de logística personalizado a las operaciones, minimizando los 
-            procedimientos administrativos o burocráticos que pudieran hacer más lenta la adquisición de alguna herramienta, personal o equipo.</h3>
+            <h3 className="captionSlide">Sistema de logística personalizado deacuerdo con las operaciones del cliente. </h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -37,6 +37,14 @@ export const AcercaDe = () => {
           <Carousel.Caption>
             <h3 className="captionSlide">Disponibilidad de todas las herramientas en sitio: 3 sets de 
             herramientas para la perforación de cada etapa.</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className="slideImgs" src={eficaz} alt="First slide" />
+          <Carousel.Caption>
+            <h3 className="captionSlide">
+            Minimizamos los procesos administrativos que pudieran hacer más lenta la adquisición de alguna herramienta, personal o equipo.</h3>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
